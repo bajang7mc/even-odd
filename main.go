bea71c52822080c74406e8a0589150eade09f452
Jay Kukadiya <48913659+jaykukadiya@users.noreply.github.com>
2020-10-01 22:40:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,30 +4,13 @@ import "fmt"
 
 func main() {
 
-	//reader := bufio.NewReader(os.Stdin)
-	//fmt.Print("Enter text: ")
-	//text, _ := reader.ReadString('\n')
-	//text = strings.Replace(text, "\n", "", -1)
-	//times, _ := strconv.Atoi(text)
-
-	//fmt.Println(times)
-	//fmt.Println(text)
-
-	//for i := 0; i <= times[]; i++ {
-	//	if i%2 == 0 {
-	//		fmt.Println(i, "is even")
-	//	} else {
-	//		fmt.Println(i, "is odd")
-	//	}
-	//}
-
 	times := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	for _, time := range times {
 		if time%2 == 0 {
-			fmt.Println(time, "is even")
+			fmt.Println(time, "is Even")
 		} else {
-			fmt.Println(time, "is odd")
+			fmt.Println(time, "is Odd")
 		}
 	}
 
